9f59f693a5d43234dc586648d7ba679bc7383077
Wenji Ma <wenjima821@gmail.com>
2015-10-09 14:36:21 -0400

    Update doTRA.sh

--- doTRA.sh
+++ doTRA.sh
@@ -10,5 +10,5 @@ sample=$datapath/$prefix*.bam
 samtools view -h $sample > `basename $sample .bam`.sam
 perl ReadSam.TRA.pl $outdir `basename $sample .bam`.sam 2>/dev/null
 perl OutputCDR3prot.TRAbyscore.pl $outdir `basename $sample .bam`.sam.CDR3.VJ.seq 2>/dev/null
-rm `basename $sample .bam`.sam
+# rm `basename $sample .bam`.sam
 
